8d967a1aaef8bd158920a4f7a2c0055af12743da
shayna chhari <shaynachhari@gmail.com>
2024-02-22 20:41:45 +0530

    first commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,70 @@
+class ItemsController < ApplicationController
+ 
+  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  load_and_authorize_resource
+
+
+  # GET /items or /items.json
+  def index
+    @items = Item.all
+  end
+
+  # GET /items/1 or /items/1.json
+  def show
+  end
+
+  # GET /items/new
+  
+  # GET /items/1/edit
+ 
+  # POST /items or /items.json
+  def create
+    # def create
+  # @item = Item.new(item_params)
+  @item.user_id = current_user.id
+
+    # @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /items/1 or /items/1.json
+  def update
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /items/1 or /items/1.json
+  def destroy
+    @item.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    
+
+    # Only allow a list of trusted parameters through.
+    def item_params
+      params.require(:item).permit(:name, :description, :price, :user_id)
+    end
+end
